84952f33f3489d592ee74d8ec53f66460d379b8b
jonny <enjoy.hyun@gmail.com>
2021-08-25 21:36:59 +0900

    og:image 경로 수정

--- p/admin/assets/js/meta.js
+++ p/admin/assets/js/meta.js
@@ -2,7 +2,7 @@ const metaTag = `
 <meta property="og:title" content="CSJ 관리자 사이트" />
 <meta property="og:type" content="website" />
 <meta property="og:url" content="http://template.chosajang.com/p/admin" />
-<meta property="og:image" content="http://template.chosajang.com/assets/images/blog-admin.png" />
+<meta property="og:image" content="http://template.chosajang.com/assets/images/blog-admin.jpg" />
 <meta property="og:description" content="Tailwind CSS 2로 제작한 관리자 템플릿" />`;
 
 document.write( metaTag );

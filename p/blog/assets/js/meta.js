68d9acc10dba36162a46f05804c95fd998127aba
jonny <enjoy.hyun@gmail.com>
2021-08-25 21:30:55 +0900

    블로그, 관리자 opengraph 설정

---
+++ p/blog/assets/js/meta.js
@@ -0,0 +1,8 @@
+const metaTag = `
+<meta property="og:title" content="CSJ 블로그 사이트" />
+<meta property="og:type" content="website" />
+<meta property="og:url" content="http://template.chosajang.com/p/blog" />
+<meta property="og:image" content="http://template.chosajang.com/assets/images/blog-service.png" />
+<meta property="og:description" content="Tailwind CSS 2로 제작한 블로그 템플릿" />`;
+
+document.write( metaTag );

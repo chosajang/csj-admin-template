836e3392f82aff89c8a867dffaf152cd7fe625f7
jonny <enjoy.hyun@gmail.com>
2021-12-05 18:46:05 +0900

    서브메뉴 추가

--- p/web-console/assets/js/sidebar.js
+++ p/web-console/assets/js/sidebar.js
@@ -8,15 +8,22 @@ const asideHTML = `
 </div> -->
 <!--// Left Menu : ST -->
 <div>
-  <ul class="select-none" id="menu-ext">
-    <li class="pt-4 py-2 pl-4"><p class="text-xs font-bold text-gray-300">기본 메뉴</p></li>
-    <a href="${documentRoot}"><li class="py-3 pl-4 cursor-pointer hover:bg-gray-600 border-l-4 border-transparent hover:border-blue-500"><i class="fas fa-columns mr-4"></i>대시보드</li></a>
+  <ul class="select-none mt-4" id="menu-ext">
+<!--    <li class="pt-4 py-2 pl-4"><p class="text-xs font-bold text-gray-300">기본 메뉴</p></li>-->
+    <a href="${documentRoot}"><li class="py-3 pl-4 text-xs border-b border-gray-700 hover:text-white cursor-pointer hover:bg-gray-700"><i class="fas fa-columns w-4 mr-2"></i>대시보드</li></a>
+    <li class="py-3 pl-4 text-xs border-b border-gray-700 hover:text-white cursor-pointer hover:bg-gray-700"><i class="fas fa-users w-4 mr-2"></i>계정관리</li>
+    <ul>
+      <li class="py-2 pl-6 hover:text-white cursor-pointer hover:bg-gray-700">test</li>
+      <li class="py-2 pl-6 hover:text-white cursor-pointer hover:bg-gray-700">test</li>
+      <li class="py-2 pl-6 hover:text-white cursor-pointer hover:bg-gray-700">test</li>
+    </ul>
   </ul>
 </div><!--// Left Menu : ED -->
 
 <!--// Project Exit : ST -->
 <div class="w-full absolute bottom-0 bg-blue-500 text-center flex items-center justify-center h-14 cursor-pointer" onclick="location.href='/'">
-    <span class="mr-2 text-lg">프로젝트 나가기</span><img src="/p/admin/assets/images/exit-64.png" class="w-14" alt="Exit" />
+  <span class="text-white mr-2 text-lg">프로젝트 나가기</span>
+  <img src="/p/admin/assets/images/exit-64.png" class="w-14" alt="Exit" />
 </div><!--// Project Exit : ED -->
 `;
 
@@ -26,7 +33,7 @@ const asideHTML = `
 document.addEventListener("DOMContentLoaded", function(){
   let asideObj = document.getElementById('aside-menu');
   asideObj.classList.add('fixed','top-0','left-0','z-40','w-52','pt-12','h-screen');
-  asideObj.classList.add('bg-gray-800','text-gray-200','text-sm');
+  asideObj.classList.add('bg-gray-800','text-gray-400','text-sm');
   asideObj.classList.add('items-center','overflow-y-auto','flex-row','flex-no-wrap','overflow-hidden','shadow-md');
   asideObj.classList.add('transform','-translate-x-52','md:translate-x-0','ease-out','duration-200');
   asideObj.innerHTML = asideHTML;
